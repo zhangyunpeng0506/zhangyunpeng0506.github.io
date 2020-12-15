e8342c256575386879ed111857b497758c9eb540
zhangyunpeng <zhangyunpeng0506@163.com>
2020-12-15 16:04:05 +0800

    css、js、vue模块整体布局

--- src/pages/questions/css_/index.vue
+++ src/pages/questions/css_/index.vue
@@ -1,13 +1,49 @@
 <template>
-  <div>css</div>
+  <div class="css-container custom-scrollbar">
+    <el-collapse v-model="activeName" accordion>
+      <template v-for="(question, index) in questions">
+        <el-collapse-item :key="'question_'+index" :name="index">
+          <template slot="title">
+            <div :class="{divider:activeName === index}" class="title">
+              {{ question }}
+            </div>
+          </template>
+        </el-collapse-item>
+      </template>
+    </el-collapse>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      questions: this.$store.state.questions.css,
+      activeName: ''
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+  .css-container{
+    height: 100%;
+    .title{
+      font-size: 15px;
+      padding-left: 5px;
+      font-weight: 700;
+      width: 100%;
+    }
+    .divider{
+      border-bottom: 1px dashed #ddd;
+    }
+    .content{
+      padding-left: 10px;
+      padding-top: 10px;
+      .cmd{
+        margin-right: 15px;
+        font-weight: 700;
+      }
+    }
+  }
 </style>
--- src/store/modules/questions.js
+++ src/store/modules/questions.js
@@ -10,6 +10,59 @@ const questions = {
       'cookie、token',
       'http与https',
       'http状态码'
+    ],
+    html: [
+      'h5新特性'
+    ],
+    css: [
+      '@media',
+      '垂直居中',
+      '多行文本省略',
+      '盒子模型',
+      '块级元素和行内元素',
+      '浏览器渲染全过程（含重绘重排）',
+      '清除浮动',
+      '优雅降级和渐进增强',
+      '元素消失的方法及区别',
+      'BFC',
+      'css3新特性',
+      'css布局',
+      'css选择器优先级',
+      'flex布局',
+      'line-height与height',
+      'link和import标签区别',
+      'position属性比较',
+      'transition和animation的区别'
+    ],
+    js: [
+      '常见题目',
+      '闭包',
+      '常用数据结构',
+      '防抖、节流',
+      '高阶函数',
+      '内存泄露',
+      '深拷贝',
+      '事件委托（事件代理）',
+      '性能优化',
+      '原型对象和原型链',
+      '暂时性死区',
+      'cookie、localStorage、sessionStorage',
+      'event-loop',
+      'js数据类型及判断',
+      'js语言特点',
+      'requestAnimationFrame'
+    ],
+    vue: [
+      '函数式组件',
+      '路由守卫',
+      '设计模式',
+      '数据双向绑定原理',
+      '虚拟DOM',
+      'diff算法及key的作用',
+      'nextTick',
+      'provide、inject',
+      'scoped、deep',
+      'virtual DOM'
     ]
   }
 }
