c496bc02d72162ef9ee387d30a632923cad31d42
zhangyunpeng <zhangyunpeng0506@163.com>
2020-12-21 17:46:29 +0800

    BFC

---
+++ src/pages/questions/css_/components/BFC/example.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="example-container">
+    <div class="parent1">
+      <div class="child1">
+        未使用BFC，发生越界<br>
+        margin-top: 5px
+      </div>
+      margin-top: 5px
+    </div>
+    <div class="parent2">
+      <div class="child21">
+        未使用BFC，发生塌陷<br>
+        float: right
+      </div>
+      <div class="child22">
+        参考
+      </div>
+    </div>
+    <div class="parent3">
+      <div class="child3" style="float:right;height:200px">float:right</div>
+      <div class="child3" style="margin:5px">margin:5px</div>
+      使用BFC，未发生塌陷和margin越界
+      <div class="child3">参考</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+  .example-container{
+    padding: 5px;
+    .parent1{
+      height: 100px;
+      width: 300px;
+      background: red;
+      margin-top: 5px;
+      .child1{
+        height: 70px;
+        width: 150px;
+        margin-top: 5px;
+        background: greenyellow;
+      }
+    }
+    .parent2{
+      background: red;
+      padding: 5px;
+      margin-top: 10px;
+      width: 300px;
+      .child21{
+        height: 70px;
+        width: 150px;
+        background: greenyellow;
+        float: right;
+      }
+      .child22{
+        height: 50px;
+        width: 50px;
+        background: greenyellow;
+        margin:2px
+      }
+    }
+    .parent3{
+      background: red;
+      width: 300px;
+      display: inline-block;
+      margin-top: 5px;
+      .child3{
+        width: 70px;
+        height: 70px;
+        background: greenyellow;
+      }
+    }
+  }
+</style>
--- src/pages/questions/css_/components/BFC/index.vue
+++ src/pages/questions/css_/components/BFC/index.vue
@@ -1,10 +1,54 @@
 <template>
-  <div>BFC</div>
+  <div>
+    <p>
+      <b>定义：</b><span>{{ description }}</span>
+    </p>
+    <p>
+      <b>特性：</b>
+      <ul>
+        <template v-for="(item,index) in character">
+          <ol :key="'character_'+index">{{ index+1 }}、{{ item }}</ol>
+        </template>
+      </ul>
+    </p>
+    <p>
+      <b>创建BFC：</b>
+      <ul>
+        <template v-for="(item,index) in createMethods">
+          <ol :key="'method_'+index">{{ index+1 }}、{{ item }}</ol>
+        </template>
+      </ul>
+    </p>
+    <example/>
+  </div>
 </template>
 
 <script>
+import example from './example'
 export default {
-
+  components: {
+    example
+  },
+  data() {
+    return {
+      description: '块级格式化上下文，是web页面中盒模型布局的css渲染模式，指一个独立的渲染区域或者说是一个独立容器',
+      character: [
+        '内部的box会在垂直方向上从顶部开始一个接一个放置',
+        '属于同一BFC的box的margin相互叠加（谁的margin值大，就是谁的margin，一正一负则是两者相加的结果）',
+        '不会出现margin越界的问题（即子元素margin跑到父元素外）',
+        '容器里面的子元素不会影响到外面的元素',
+        '计算高度时，浮动子元素也参与计算（解决设置子元素为浮动后，父元素高度发生塌陷的问题）',
+        '区域不会与float box重叠'
+      ],
+      createMethods: [
+        'body',
+        'float设置除none以外的值',
+        'position设置absolute / fixed',
+        'display设置table-cell / table-caption / inline-block / flex / inline-flex',
+        'overflow设置除visible以外的值'
+      ]
+    }
+  }
 }
 </script>
 
