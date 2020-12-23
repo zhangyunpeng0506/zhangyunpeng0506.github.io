03c82d0f44b6e3d2b4038ba9fda3ded625f1cdbf
zhangyunpeng <zhangyunpeng0506@163.com>
2020-12-23 14:08:38 +0800

    盒子模型

--- src/pages/questions/css_/components/盒子模型/index.vue
+++ src/pages/questions/css_/components/盒子模型/index.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>盒子模型</div>
+  <div class="box-container">
+    <p>
+      <b>概念：</b>{{ define }}
+    </p>
+    <p>
+      <b>宽度和高度：</b>{{ des }}
+    </p>
+    <p>
+      <b>box-sizing：</b>content-box / border-box
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      define: 'CSS盒子模型（Box Model）本质上是一个盒子，包括外边距（margin），边框（border）、内边距（padding）、内容（content）',
+      des: '标准盒子模型的width和height表示content的宽度或高度，IE盒子模型的width和height表示content+padding+border'
+    }
+  }
 }
 </script>
 
