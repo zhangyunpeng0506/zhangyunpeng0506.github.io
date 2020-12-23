0faab302a1ce281da17a1428e5d4b22cea85a908
zhangyunpeng <zhangyunpeng0506@163.com>
2020-12-23 16:51:27 +0800

    margin越界

---
+++ src/pages/questions/css_/components/margin越界/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <b>解决方案：</b>
+    <ul>
+      <template v-for="(item, index) in methodsList">
+        <ol :key="'method_' + index">{{ index + 1 }}、{{ item }}</ol>
+      </template>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      methodsList: [
+        '给parent设置border',
+        '给parent设置padding',
+        '给parent设置overflow:hidden（原理：BFC）',
+        `给parent加前置伪元素::before{ content:''; display: table }`
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
